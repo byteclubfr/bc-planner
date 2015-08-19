b82085eb9b2e7d7596d55ea635b9ad1f33e804ec
Nicolas Chambrier <naholyr@gmail.com>
2015-08-19 16:25:03 +0200

    Background color on filters

--- app/components/filters.jsx
+++ app/components/filters.jsx
@@ -6,13 +6,21 @@ import clubbers from './../constants/clubbers'
 import Gravatar from './gravatar'
 
 export default class Filters extends Component {
+  clubberCheckbox (clubber, name) {
+    return (
+      <label key={name} style={{backgroundColor: clubber.color}}>
+        <input type="checkbox" />
+        <Gravatar clubberName={name} />
+        {name}
+      </label>
+    )
+  }
+
   render () {
     return (
       <form className="filters">
         <strong>Filter by clubber</strong>
-        {clubbers.map((clubber, name) =>
-          <label key={name}><input type="checkbox" /><Gravatar clubberName={name} /> {name}</label>
-        ).toArray()}
+        {clubbers.map(::this.clubberCheckbox).toArray()}
       </form>
     )
   }
--- app/constants/clubbers.js
+++ app/constants/clubbers.js
@@ -3,14 +3,14 @@ import { Map } from 'immutable'
 export default Map({
   bruno: {
     gravatar: '1de6337421c1eac0227c88502b275952',
-    color: '#ffc'
+    color: '#eec'
   },
   nicolas: {
     gravatar: '396445b6e14727dcb6b3d6a66d52b567',
-    color: '#cff'
+    color: '#cee'
   },
   thomas: {
     gravatar: '00045a0899ce378f822f64b62a284029',
-    color: '#fcf'
+    color: '#ece'
   }
 })
