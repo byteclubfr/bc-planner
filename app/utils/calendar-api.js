2dd02918f795886b7d4aeaffc423aa9e6b17a05a
Delapouite <delapouite@gmail.com>
2015-08-19 10:01:56 +0200

    chore(eslint): declare indents, quote and semi rules

--- app/utils/calendar-api.js
+++ app/utils/calendar-api.js
@@ -3,51 +3,51 @@ import uuid from 'uuid'
 const sampleEvents = [
   {
     id: uuid(),
-    type: "formation-intra",
+    type: 'formation-intra',
     confirmed: true,
-    start: "2015-09-10",
-    end: "2015-09-12",
-    clubber: "Nicolas",
-    subject: "Node",
-    place: "Paris",
+    start: '2015-09-10',
+    end: '2015-09-12',
+    clubber: 'Nicolas',
+    subject: 'Node',
+    place: 'Paris',
     // TODO generate
-    title: "INTRA Node Paris"
+    title: 'INTRA Node Paris'
   },
   {
     id: uuid(),
-    type: "formation-inter",
+    type: 'formation-inter',
     confirmed: true,
-    start: "2015-09-15",
-    end: "2015-09-17",
-    clubber: "Bruno",
-    subject: "Angular",
-    place: "Paris",
+    start: '2015-09-15',
+    end: '2015-09-17',
+    clubber: 'Bruno',
+    subject: 'Angular',
+    place: 'Paris',
     // TODO generate
-    title: "INTER Angular Paris"
+    title: 'INTER Angular Paris'
   },
   {
     id: uuid(),
-    type: "formation-inter",
+    type: 'formation-inter',
     confirmed: false,
-    start: "2015-10-21",
-    end: "2015-10-22",
-    subject: "React",
-    place: "Toulouse",
+    start: '2015-10-21',
+    end: '2015-10-22',
+    subject: 'React',
+    place: 'Toulouse',
     clubber: null,
     // TODO generate
-    title: "INTER React Toulouse"
+    title: 'INTER React Toulouse'
   },
   {
     id: uuid(),
-    type: "dev",
+    type: 'dev',
     confirmed: false,
-    start: "2015-10-30",
-    end: "2015-11-10",
-    subject: "Immobox",
+    start: '2015-10-30',
+    end: '2015-11-10',
+    subject: 'Immobox',
     place: null,
-    clubber: "Thomas",
+    clubber: 'Thomas',
     // TODO generate
-    title: "DEV Immobox"
+    title: 'DEV Immobox'
   }
 ]
 
