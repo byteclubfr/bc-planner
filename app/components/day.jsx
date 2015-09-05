2a3f630b6651674e671545742b325be5f14a3710
Delapouite <delapouite@gmail.com>
2015-09-05 15:32:52 +0200

    refactor: move event to own component

--- app/components/day.jsx
+++ app/components/day.jsx
@@ -3,9 +3,8 @@ import '../styles/day'
 import React, { Component, PropTypes } from 'react'
 import classNames from 'classnames'
 import { Map, Set } from 'immutable'
-import moment from 'moment'
 
-import Gravatar from './gravatar'
+import Event from './event'
 import EventBars from './event-bars'
 import { inclusiveIsBetween, isWeekend } from './../utils/date'
 
@@ -20,84 +19,8 @@ export default class Day extends Component {
     withTags: PropTypes.instanceOf(Set).isRequired
   }
 
-  renderEvent (event) {
-    const { filters } = this.props
-    return (
-      <li className="event" key={event.id} onClick={() => this.props.actions.openEventForm(event.id)}>
-        {this.renderPopOver(event)}
-        <div className="event-snippet">
-          {filters.get('title') ? this.renderTitle(event) : null}
-          {filters.get('location') ? this.renderLocation(event) : null}
-          {filters.get('tags') ? this.renderTags(event) : null}
-        </div>
-        {filters.get('gravatar') ? this.renderGravatars(event) : null}
-      </li>
-    )
-  }
-
-  renderTitle (event) {
-    return (
-      <div className="event-title">
-        <span className="event-confirmed" title="Event confirmed?">
-          {event._confirmed ? '✓' : '✗'}
-        </span>
-        <span>{event.title}</span>
-      </div>
-    )
-  }
-
-  renderLocation (event) {
-    return <div className="event-location">{event.location}</div>
-  }
-
-  renderTags (event) {
-    if (!event._tags) return null
-
-    return <div className="event-tags">{event._tags.sort().join(', ')}</div>
-  }
-
-  renderGravatars (event) {
-    let clubbers = event._clubbers.filter(c => this.props.visibleClubbers.includes(c))
-
-    return (
-      <div className="event-gravatars">
-        {(clubbers.map(clubber => {
-          return (
-            <span className="event-gravatar" key={clubber}>
-              <Gravatar email={clubber} />
-            </span>
-          )
-        }))}
-      </div>
-    )
-  }
-
-  renderDates (event) {
-    let f = 'ddd D MMM'
-
-    return (
-      <div className="event-dates">
-        {moment(event.start).format(f)}
-        {event.start !== event.end ? ' - ' + moment(event.end).format(f) : null}
-      </div>
-    )
-  }
-
-  renderPopOver (event) {
-    return (
-      <div className="event-pop-over">
-        {this.renderTitle(event)}
-        {this.renderDates(event)}
-        {this.renderGravatars(event)}
-        {this.renderLocation(event)}
-        {this.renderTags(event)}
-        <div className="event-description">{event.description}</div>
-      </div>
-    )
-  }
-
   render () {
-    const { date, filters, withTags, visibleClubbers } = this.props
+    const { actions, date, filters, withTags, visibleClubbers } = this.props
     const events = this.props.events
       .filter(event =>
         inclusiveIsBetween(date, event.start, event.end)
@@ -113,7 +36,7 @@ export default class Day extends Component {
       <div className={classNames('day', { 'day-weekend': isWeekend(date) })}>
         <header className="day-date">{date.format('dd')[0]} {date.format('DD')}</header>
         <ul className="event-list">
-          {events.map(::this.renderEvent)}
+          {events.map(event => <Event action={actions} event={event} filters={filters} visibleClubbers={visibleClubbers} />)}
         </ul>
         {filters.get('bars') ? <EventBars events={events} visibleClubbers={visibleClubbers} /> : null}
       </div>
---
+++ app/components/event.jsx
@@ -0,0 +1,92 @@
+import React, { Component, PropTypes } from 'react'
+import moment from 'moment'
+
+import Gravatar from './gravatar'
+
+export default class Event extends Component {
+
+  static propTypes = {
+    actions: PropTypes.object.isRequired,
+    event: PropTypes.object.isRequired,
+    filters: PropTypes.instanceOf(Map).isRequired,
+    visibleClubbers: PropTypes.instanceOf(Set).isRequired
+  }
+
+  renderTitle (event) {
+    return (
+      <div className="event-title">
+        <span className="event-confirmed" title="Event confirmed?">
+          {event._confirmed ? '✓' : '✗'}
+        </span>
+        <span>{event.title}</span>
+      </div>
+    )
+  }
+
+  renderLocation (event) {
+    return <div className="event-location">{event.location}</div>
+  }
+
+  renderTags (event) {
+    if (!event._tags) return null
+
+    return <div className="event-tags">{event._tags.sort().join(', ')}</div>
+  }
+
+  renderGravatars (event) {
+    let clubbers = event._clubbers.filter(c => this.props.visibleClubbers.includes(c))
+
+    return (
+      <div className="event-gravatars">
+        {(clubbers.map(clubber => {
+          return (
+            <span className="event-gravatar" key={clubber}>
+              <Gravatar email={clubber} />
+            </span>
+          )
+        }))}
+      </div>
+    )
+  }
+
+  renderDates (event) {
+    let f = 'ddd D MMM'
+
+    return (
+      <div className="event-dates">
+        {moment(event.start).format(f)}
+        {event.start !== event.end ? ' - ' + moment(event.end).format(f) : null}
+      </div>
+    )
+  }
+
+  renderPopOver (event) {
+    return (
+      <div className="event-pop-over">
+        {this.renderTitle(event)}
+        {this.renderDates(event)}
+        {this.renderGravatars(event)}
+        {this.renderLocation(event)}
+        {this.renderTags(event)}
+        <div className="event-description">{event.description}</div>
+      </div>
+    )
+  }
+
+  render () {
+    const { actions, event, filters } = this.props
+
+    return (
+      <li className="event" key={event.id} onClick={() => actions.openEventForm(event.id)}>
+        {this.renderPopOver(event)}
+        <div className="event-snippet">
+          {filters.get('title') ? this.renderTitle(event) : null}
+          {filters.get('location') ? this.renderLocation(event) : null}
+          {filters.get('tags') ? this.renderTags(event) : null}
+        </div>
+        {filters.get('gravatar') ? this.renderGravatars(event) : null}
+      </li>
+    )
+  }
+
+}
