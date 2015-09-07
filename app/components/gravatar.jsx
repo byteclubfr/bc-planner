3efaf01ad46a36dc642b2ad74b9f52af8efbfe12
Delapouite <delapouite@gmail.com>
2015-09-07 22:35:38 +0200

    feat: perf - add shouldComponentUpdate to <Gravatar>

--- app/components/gravatar.jsx
+++ app/components/gravatar.jsx
@@ -1,10 +1,12 @@
-import '../styles/day'
-
 import React, { Component } from 'react'
 
 import clubbers from './../constants/clubbers'
 
 export default class Gravatar extends Component {
+
+  shouldComponentUpdate (nextProps) {
+    return this.props.email !== nextProps.email
+  }
 
   render () {
     const { email } = this.props
