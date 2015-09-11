3d4172b4b636b2b072b5cdd3bcd6385d1cf44a48
Delapouite <delapouite@gmail.com>
2015-09-11 11:02:52 +0200

    refactor: move events formatters to utils

--- app/calendar-api.js
+++ app/calendar-api.js
@@ -1,84 +1,8 @@
-import moment from 'moment'
-import { mapKeys, mapValues } from 'lodash/object'
-
 import { calendarId, clientId, scopes } from './constants/google-credentials'
 import store from './store'
 import { fetchEvents, setOnline } from './actions'
+import { shapeClientEvent, shapeServerEvent } from './utils/event'
 
-
-function getTitle (event) {
-  var title = event.summary
-  return title
-}
-
-// boundary: 'start' || 'end'
-function getDate (event, boundary) {
-  var d
-  var date = event[boundary]
-  // all day event
-  if (date.date) {
-    // start is inclusive, end is exclusive
-    d = date.date
-    if (boundary === 'end') {
-      d = moment(d).subtract(1, 'days')
-    }
-  } else {
-    d = date.dateTime
-  }
-  return moment(d).format('YYYY-MM-DD')
-}
-
-// based on attendees for now
-function getClubber (event) {
-  if (!event.attendees) return ['lilian.martineau@gmail.com']
-
-  return event.attendees
-    .filter(a => !a.optionnal)
-    .map(a => a.email)
-}
-
-function extendedPropertiesToUnder (event) {
-  if (!event.extendedProperties) return event
-
-  let exProps = mapValues(event.extendedProperties.shared, JSON.parse)
-  exProps = mapKeys(exProps, (v, k) => '_' + k)
-  return {...event, ...exProps}
-}
-
-// TODO - remove
-// format server event -> client event
-function shapeServerEvent (event) {
-  event.title = getTitle(event)
-  event.start = getDate(event, 'start')
-  event.end = getDate(event, 'end')
-
-  // client only props with under
-  event._clubbers = getClubber(event)
-  event = extendedPropertiesToUnder(event)
-  return event
-}
-
-// format form event -> server event
-function shapeClientEvent (event) {
-  event = { ...event }
-  event.start = { date: event.start }
-  event.end = { date: moment(event.end).add(1, 'days').format('YYYY-MM-DD') }
-  if (!event.extendedProperties) event.extendedProperties = {}
-
-  // move back under props in shared
-  event.extendedProperties.shared = {
-    ...event.extendedProperties.shared,
-    confirmed: event._confirmed,
-    tags: JSON.stringify(event._tags)
-  }
-
-  // TODO
-  delete event._clubbers
-  delete event._confirmed
-  delete event._tags
-
-  return event
-}
 
 function initialFetch () {
   store.dispatch(setOnline())
---
+++ app/utils/event.js
@@ -0,0 +1,81 @@
+import moment from 'moment'
+import { mapKeys, mapValues } from 'lodash/object'
+
+// the following fns are used as translators between events shaped by Google
+// and the ones we need client side
+
+
+// TODO better title construction
+export function getTitle (event) {
+  var title = event.summary
+  return title
+}
+
+// boundary: 'start' || 'end'
+export function getDate (event, boundary) {
+  var d
+  var date = event[boundary]
+  // all day event
+  if (date.date) {
+    // start is inclusive, end is exclusive
+    d = date.date
+    if (boundary === 'end') {
+      d = moment(d).subtract(1, 'days')
+    }
+  } else {
+    d = date.dateTime
+  }
+  return moment(d).format('YYYY-MM-DD')
+}
+
+// based on attendees for now
+export function getClubber (event) {
+  if (!event.attendees) return ['lilian.martineau@gmail.com']
+
+  return event.attendees
+    .filter(a => !a.optionnal)
+    .map(a => a.email)
+}
+
+export function extendedPropertiesToUnder (event) {
+  if (!event.extendedProperties) return event
+
+  let exProps = mapValues(event.extendedProperties.shared, JSON.parse)
+  exProps = mapKeys(exProps, (v, k) => '_' + k)
+  return {...event, ...exProps}
+}
+
+// TODO - remove
+// format server event -> client event
+export function shapeServerEvent (event) {
+  event.title = getTitle(event)
+  event.start = getDate(event, 'start')
+  event.end = getDate(event, 'end')
+
+  // client only props with under
+  event._clubbers = getClubber(event)
+  event = extendedPropertiesToUnder(event)
+  return event
+}
+
+// format form event -> server event
+export function shapeClientEvent (event) {
+  event = { ...event }
+  event.start = { date: event.start }
+  event.end = { date: moment(event.end).add(1, 'days').format('YYYY-MM-DD') }
+  if (!event.extendedProperties) event.extendedProperties = {}
+
+  // move back under props in shared
+  event.extendedProperties.shared = {
+    ...event.extendedProperties.shared,
+    confirmed: event._confirmed,
+    tags: JSON.stringify(event._tags)
+  }
+
+  // TODO
+  delete event._clubbers
+  delete event._confirmed
+  delete event._tags
+
+  return event
+}
