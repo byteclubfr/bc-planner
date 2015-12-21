62913dd625a6e427a8c549e86918fee2b03d4cb1
Delapouite <delapouite@gmail.com>
2015-12-21 12:14:53 +0100

    refactor: remove deprecated redux-action-utils in favor of plain ES6

--- app/actions/index.js
+++ app/actions/index.js
@@ -1,5 +1,3 @@
-import { actionCreator } from 'redux-action-utils'
-
 import * as types from '../constants/actions'
 import calendar from '../calendar-api'
 
@@ -10,7 +8,7 @@ export function openEventForm (eventId = null) {
   }
 }
 
-export const closeEventForm = actionCreator(types.UI_CLOSE_EVENT_FORM)
+export const closeEventForm = () => ({ type: types.UI_CLOSE_EVENT_FORM })
 
 export function createEvent (formData) {
   return dispatch => {
@@ -19,7 +17,7 @@ export function createEvent (formData) {
   }
 }
 
-export const createdEvent = actionCreator(types.CREATED_EVENT, 'event')
+export const createdEvent = (event) => ({ type: types.CREATED_EVENT, event })
 
 export function updateEvent (eventId, formData) {
   return dispatch => {
@@ -28,7 +26,7 @@ export function updateEvent (eventId, formData) {
   }
 }
 
-export const updatedEvent = actionCreator(types.UPDATED_EVENT, 'event')
+export const updatedEvent = (event) => ({ type: types.UPDATED_EVENT, event })
 
 export function deleteEvent (eventId) {
   return dispatch => {
@@ -37,7 +35,7 @@ export function deleteEvent (eventId) {
   }
 }
 
-export const deletedEvent = actionCreator(types.DELETED_EVENT, 'eventId')
+export const deletedEvent = (eventId) => ({ type: types.DELETED_EVENT, eventId })
 
 export function fetchEvents (options) {
   return dispatch => {
@@ -46,25 +44,25 @@ export function fetchEvents (options) {
   }
 }
 
-export const fetchedEvents = actionCreator(types.FETCHED_EVENTS, 'events')
+export const fetchedEvents = (events) => ({ type: types.FETCHED_EVENTS, events })
 
-export const toggleClubber = actionCreator(types.UI_TOGGLE_CLUBBER, 'clubber')
+export const toggleClubber = (clubber) => ({ type: types.UI_TOGGLE_CLUBBER, clubber })
 
-export const toggleFilter = actionCreator(types.UI_TOGGLE_FILTER, 'filter')
+export const toggleFilter = (filter) => ({ type: types.UI_TOGGLE_FILTER, filter })
 
-export const changeNbMonths = actionCreator(types.UI_CHANGE_NB_MONTHS, 'nbMonths')
+export const changeNbMonths = (nbMonths) => ({ type: types.UI_CHANGE_NB_MONTHS, nbMonths })
 
 // way = previous or next
-export const changeStartMonth = actionCreator(types.UI_CHANGE_START_MONTH, 'way')
+export const changeStartMonth = (way) => ({ type: types.UI_CHANGE_START_MONTH, way })
 
-export const changeConfirmed = actionCreator(types.UI_CHANGE_CONFIRMED, 'confirmed')
+export const changeConfirmed = (confirmed) => ({ type: types.UI_CHANGE_CONFIRMED, confirmed })
 
-export const changeLastUpdate = actionCreator(types.UI_CHANGE_LAST_UPDATE, 'lastUpdate')
+export const changeLastUpdate = (lastUpdate) => ({ type: types.UI_CHANGE_LAST_UPDATE, lastUpdate })
 
-export const toggleTag = actionCreator(types.UI_TOGGLE_TAG, 'tag')
+export const toggleTag = (tag) => ({ type: types.UI_TOGGLE_TAG, tag })
 
-export const search = actionCreator(types.UI_SEARCH, 'search')
+export const search = (search) => ({ type: types.UI_SEARCH, search })
 
-export const setOnline = actionCreator(types.UI_SET_ONLINE)
+export const setOnline = () => ({ type: types.UI_SET_ONLINE })
 
-export const setOffline = actionCreator(types.UI_SET_OFFLINE)
+export const setOffline = () => ({ type: types.UI_SET_OFFLINE })
