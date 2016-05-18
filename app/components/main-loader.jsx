9b94b5e2c18533e3fb4e0d206abcda7b1535fa8b
Nicolas Chambrier <naholyr@gmail.com>
2016-05-18 16:40:51 +0200

    refactor: MainLoader now a never-updated component

--- app/components/main-loader.jsx
+++ app/components/main-loader.jsx
@@ -2,10 +2,25 @@ import 'loaders.css/loaders.css'
 import React from 'react'
 import { Loader } from 'react-loaders'
 
-export default () =>
-  <div className="main-loader">
-    <div className="main-loader-message">
-      <h1>Fetching Google events...</h1>
-      <Loader type="ball-pulse-sync" />
-    </div>
-  </div>
+export default class MainLoader extends React.Component {
+
+  constructor (props) {
+    super(props)
+  }
+
+  shouldComponentUpdate () {
+    return false
+  }
+
+  render () {
+    return (
+      <div className="main-loader">
+        <div className="main-loader-message">
+          <h1>Fetching Google events...</h1>
+          <Loader type="ball-pulse-sync" />
+        </div>
+      </div>
+    )
+  }
+
+}
