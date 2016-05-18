f144123023b45f73ee10610ac20c1e79921d166b
Nicolas Chambrier <naholyr@gmail.com>
2016-05-18 21:12:19 +0200

    deps: remove why-did-you-update (too verbose)

--- app/index.js
+++ app/index.js
@@ -5,17 +5,12 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { bindActionCreators } from 'redux'
 import { Provider, connect } from 'react-redux'
-import { whyDidYouUpdate } from 'why-did-you-update'
 
 import store from './store'
 import App from './components/app'
 import * as actions from './actions'
 
 moment.locale('fr')
-
-if (process.env.NODE_ENV === 'development') {
-  whyDidYouUpdate(React, {})
-}
 
 const ConnectedApp = connect(
   ({ events, tags, ui }) => ({ events, tags, ui }),
