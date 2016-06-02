5bfdc4824f510c50a7283f825a19968f46179cd2
Delapouite <delapouite@gmail.com>
2016-06-02 19:25:16 +0200

    feat: add devToolsExtension store enhancer

--- app/store.js
+++ app/store.js
@@ -1,40 +1,47 @@
 import { compose, createStore, applyMiddleware } from 'redux'
-import thunkMiddleware from 'redux-thunk' // lets us dispatch() functions
+import thunkMiddleware from 'redux-thunk'
 import createLogger from 'redux-logger'
 import persistState from 'redux-localstorage'
 import Immutable, { Map } from 'immutable'
 
 import reducer from './reducers'
 
+// lets us dispatch() functions
+const middlewares = [thunkMiddleware]
+if (__DEV__) {
+  middlewares.push(createLogger({
+    collapsed: true,
+    // Better logging for Immutable data structure
+    stateTransformer: (state) => {
+      let newState = {}
+      for (var i of Object.keys(state)) {
+        newState[i] = Immutable.Iterable.isIterable(state[i])
+          ? state[i].toJS()
+          : state[i]
+      }
+      return newState
+    }
+  }))
+}
+
+const enhancer = compose(
+  applyMiddleware(...middlewares),
+  window.devToolsExtension ? window.devToolsExtension() : f => f
+)
+const enhancedCreateStore = enhancer(createStore)
+
+// keu to avoid conflict with other redux projects on localhost
+const KEY = 'bc-planner'
 // Initialize persistent state to avoid error when resetting data
-if (!localStorage.getItem('bc-planner')) {
-  localStorage.setItem('bc-planner', '{}')
+if (!localStorage.getItem(KEY)) {
+  localStorage.setItem(KEY, '{}')
 }
+const createPersistentStore = persistState(['events'], {
+  key: KEY,
+  deserialize: str => ({ events: Map(JSON.parse(str).events)})
+})(enhancedCreateStore)
 
-const createPersistentStore = compose(
-  persistState(['events'], {
-    // to avoid conflict with other redux projects on localhost
-    key: 'bc-planner',
-    deserialize: str => ({ events: Map(JSON.parse(str).events)})
-  }),
-)(createStore)
-
-// Better logging for Immutable data structure
-const stateTransformer = (state) => {
-  let newState = {}
-  for (var i of Object.keys(state)) {
-    newState[i] = Immutable.Iterable.isIterable(state[i])
-      ? state[i].toJS()
-      : state[i]
-  }
-  return newState
-}
-
-var middlewares = __DEV__
-  ? applyMiddleware(thunkMiddleware, createLogger({ collapsed: true, stateTransformer }))
-  : applyMiddleware(thunkMiddleware)
-
-const store = middlewares(createPersistentStore)(reducer)
+const store = createPersistentStore(reducer)
 
 if (__DEV__ && module.hot) {
   // Enable Webpack hot module replacement for reducers
