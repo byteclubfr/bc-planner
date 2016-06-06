21f149ed77485724914bd0697676879370919042
Nicolas Chambrier <naholyr@gmail.com>
2016-06-06 14:08:05 +0200

    chore(date): tests comparison methods

--- test/utils/date.js
+++ test/utils/date.js
@@ -3,8 +3,8 @@ import {
   // Tested
   serialize, addMonth, addDay, endOfMonth, startOfMonth,
   buildMonthsRange, buildMonthDaysRange,
-  // Untested
   inclusiveIsBetween, isWeekend, isToday, isAfter, isBefore, isSameDay, isSameMonth,
+  // Untested
   formatDay, formatAgo, formatMonth, formatMonthDay
 } from '../../app/utils/date'
 
@@ -33,9 +33,16 @@ describe('utils/date', () => {
       ['2011-11-11', 30, '2011-12-11'],
       ['2012-12-11', 10, '2012-12-21'],
       ['2016-12-30', 20, '2017-01-19'],
-      ['2016-10-31', 1, '2016-11-01'],
-      ['2016-11-30', 1, '2016-12-01'],
-      ['2016-12-31', 1, '2017-01-01'],
+      ['2016-10-31', 1,  '2016-11-01'],
+      ['2016-11-30', 1,  '2016-12-01'],
+      ['2016-12-31', 1,  '2017-01-01'],
+      // Remove days
+      ['2011-12-11', -30, '2011-11-11'],
+      ['2012-12-21', -10, '2012-12-11'],
+      ['2017-01-19', -20, '2016-12-30'],
+      ['2016-11-01', -1,  '2016-10-31'],
+      ['2016-12-01', -1,  '2016-11-30'],
+      ['2017-01-01', -1,  '2016-12-31'],
       // Bissextiles
       ['2016-02-29', 1, '2016-03-01'], // yep
       ['2012-02-29', 1, '2012-02-30'], // nope
@@ -122,4 +129,113 @@ describe('utils/date', () => {
       expect(result, 'days of ' + date).to.have.length(count)
     })
   })
+
+  it('inclusiveIsBetween', () => {
+    [
+      ['2011-11-11', '2012-03-01', '2012-02-29', true ], // 02-29 → 03-01 → should still be true
+      ['2011-12-11', '2011-12-11', '2011-12-11', true ],
+      ['2011-12-11', '2011-12-11', '2012-12-01', false ],
+      ['2015-02-11', '2015-02-12', '2015-02-11', true ],
+      ['2015-02-11', '2015-02-12', '2015-02-12', true ],
+      ['2015-02-11', '2015-02-12', '2015-02-13', false ],
+      ['2015-02-11', '2015-02-10', '2015-02-10', false ], // start should be lesser then end
+      ['2015-02-11', '2015-02-10', '2015-02-11', false ], // start should be lesser then end
+      ['2016-02-11', '2017-02-29', '2017-02-28', true ],
+      ['2016-02-11', '2017-02-29', '2017-02-29', true ],
+      ['2016-02-11', '2017-02-29', '2017-02-30', false ]
+    ].forEach(([start, end, date, expected]) => {
+      const result = inclusiveIsBetween(date, start, end)
+      expect(result, start + ' ≤ ' + date + ' ≤ ' + end).to.equal(expected)
+    })
+  })
+
+  it('isWeekend', () => {
+    [
+      ['2011-11-11', false], // Fri
+      ['2011-12-11', true],  // Sun
+      ['2015-02-11', false], // Wed
+      ['2016-02-11', false], // Thu
+      ['2017-01-11', false], // Wed
+      ['2011-11-30', false], // Wed
+      ['2011-12-31', true],  // Sat
+      ['2015-02-28', true],  // Sat
+      ['2016-02-29', false], // Mon
+      ['2017-01-31', false]  // Tue
+    ].forEach(([date, expected]) => {
+      const result = isWeekend(date)
+      expect(result, new Date(date)).to.equal(expected)
+    })
+  })
+
+  it('isToday', () => {
+    const today = new Date()
+    const tomorrow = addDay(today, 1)
+    const yesterday = addDay(today, -1)
+    expect(isToday(today)).to.be.true
+    expect(isToday(tomorrow)).to.be.false
+    expect(isToday(yesterday)).to.be.false
+  })
+
+  it('isAfter (first ≥ second, days only)', () => {
+    [
+      ['2011-12-31 05:00', '2011-12-31 06:00', true ], // ignore hour
+      ['2011-12-31 06:00', '2011-12-31 05:00', true ], // ignore hour
+      ['2011-12-31', '2011-12-30', true ],
+      ['2011-12-31', '2011-01-01', true ],
+      ['2011-12-31', '2012-01-01', false ],
+      ['2011-02-28', '2011-02-29', false ],
+      ['2011-02-28', '2011-01-29', true ],
+      ['2011-02-28', '2011-02-27', true ]
+    ].forEach(([ start, end, expected ]) => {
+      const result = isAfter(start, end)
+      expect(result, start + ' ≥ ' + end).to.equal(expected)
+    })
+  })
+
+  it('isBefore (first ≤ second, days only)', () => {
+    [
+      ['2011-12-31 05:00', '2011-12-31 06:00', true ], // ignore hour
+      ['2011-12-31 06:00', '2011-12-31 05:00', true ], // ignore hour
+      ['2011-12-31', '2011-12-30', false ],
+      ['2011-12-31', '2011-01-01', false ],
+      ['2011-12-31', '2012-01-01', true ],
+      ['2011-02-28', '2011-02-29', true ],
+      ['2011-02-28', '2011-01-29', false ],
+      ['2011-02-28', '2011-02-27', false ]
+    ].forEach(([ start, end, expected ]) => {
+      const result = isBefore(start, end)
+      expect(result, start + ' ≤ ' + end).to.equal(expected)
+    })
+  })
+
+  it('isSameDay (same year, month, and day)', () => {
+    [
+      ['2011-12-31 05:00', '2011-12-31 06:00', true ], // ignore hour
+      ['2011-12-31 06:00', '2011-12-31 05:00', true ], // ignore hour
+      ['2011-01-02', '2011-02-02', false], // not same month
+      ['2011-01-02', '2012-01-02', false], // not same year
+      ['2011-01-02', '2011-01-02', true],
+      ['2011-02-29', '2011-03-01', true] // converting 02-29 to 03-01
+    ].forEach(([date1, date2, expected]) => {
+      const result = isSameDay(date1, date2)
+      expect(result, date1 + ' ~ ' + date2).to.equal(expected)
+    })
+  })
+
+  it('isSameMonth (same year and month)', () => {
+    [
+      ['2011-12-31 05:00', '2011-12-31 06:00', true ], // ignore hour
+      ['2011-12-12 06:00', '2011-12-31 05:00', true ], // ignore hour and day
+      ['2011-01-02', '2011-02-02', false], // not same month
+      ['2011-01-02', '2012-01-02', false], // not same year
+      ['2011-01-02', '2011-01-02', true],
+      ['2011-01-02', '2011-01-03', true], // ignore day
+      ['2011-02-29', '2011-03-01', true] // converting 02-29 to 03-01
+    ].forEach(([date1, date2, expected]) => {
+      const result = isSameMonth(date1, date2)
+      expect(result, date1 + ' ~ ' + date2).to.equal(expected)
+    })
+
+  })
+
 })
